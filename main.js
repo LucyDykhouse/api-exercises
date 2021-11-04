52b3a67f4a683cfe60e3136d5bd619ed9e0bf5c7
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-03 21:43:41 -0400

    Finished index.html and main.js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const resBtn = document.querySelector('button');
-const buttonAction = () => {
-    console.log('The button was clicked');
+const resBtn = document.querySelector("button");
+const getPlanetInfo = () => {
+      axios.get("https://swapi.dev/api/planets/2/")
+      .then(response => {
+        //console.log(response.data);
+        for(let i = 0; i < response.data.residents.length; i++) {
+            axios.get(`${response.data.residents[i]}`)
+            .then(res => {
+                //console.log(`${res.data.name}`);
+                const header = document.createElement("h2");
+                header.textContent = res.data.name;
+                const body = document.querySelector("body")
+                body.append(header);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
 }
 
-resBtn.addEventListener('click', buttonAction);
+resBtn.addEventListener("click", getPlanetInfo);
